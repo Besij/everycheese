19799b5f5d9ac49f71e47521eca357666944a416
Besij <besij@mail.ru>
2020-08-25 21:03:38 +0700

    First pass at working Add Cheese form

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from autoslug import AutoSlugField
+from django.urls import reverse
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
 
@@ -22,3 +23,7 @@ class Cheese(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('cheeses:detail',
+                       kwargs={"slug": self.slug})
--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -3,13 +3,18 @@ from . import views
 
 app_name = "cheeses"
 urlpatterns = [
-    path(route='',
+    path(route='/',
          view=views.CheeseListView.as_view(),
          name='list'
          ),
     path(
+        route='add/',
+        view=views.CheeseCreateView.as_view(),
+        name='add'
+        ),
+    path(
         route='<slug:slug>/',
         view=views.CheeseDetailView.as_view(),
         name='detail'
-    )
+        ),
 ]
